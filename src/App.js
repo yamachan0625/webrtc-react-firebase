afc8ea15e9e561e26dc2c216cdc2473d29f0f292
yamachan0625 <napoli.080625@gmail.com>
2021-03-19 05:37:35 +0900

    install-material-ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 const getMedia = async () => {
   const constraints = { audio: true, video: true };
@@ -15,7 +16,11 @@ const getMedia = async () => {
 getMedia();
 
 const App = () => {
-  return <div>Hello, React!</div>;
+  return (
+    <Button color="primary" variant="contained">
+      Hello, React!
+    </Button>
+  );
 };
 
 export default App;
