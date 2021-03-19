0f099a2895dd3833b47718214ba922fbf189c208
yamachan0625 <napoli.080625@gmail.com>
2021-03-20 01:09:06 +0900

    create siginin page for peername

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InputFormLocal from './InputFormLocal';
 import InputFormRemote from './InputFormRemote';
 
@@ -17,10 +17,19 @@ const getMedia = async () => {
 getMedia();
 
 const App = () => {
+  const [localPeerName, setLocalPeerName] = useState('');
+  const [remotePeerName, setRemotePeerName] = useState('');
+
   return (
     <>
-      <InputFormLocal />
-      <InputFormRemote />
+      <InputFormLocal
+        localPeerName={localPeerName}
+        setLocalPeerName={setLocalPeerName}
+      />
+      <InputFormRemote
+        remotePeerName={remotePeerName}
+        setRemotePeerName={setRemotePeerName}
+      />
     </>
   );
 };
