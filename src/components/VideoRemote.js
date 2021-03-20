6c0f0d00754dd37c26c5c67c87296a02bc0c443d
yamachan0625 <napoli.080625@gmail.com>
2021-03-21 00:41:05 +0900

    remoteVideoRef

--- src/components/VideoRemote.js
+++ src/components/VideoRemote.js
@@ -1,11 +1,17 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 
 import Video from './Video';
 
-const VideoRemote = ({ name }) => {
-  const videoRef = null;
+const VideoRemote = ({ rtcClient }) => {
+  const videoRef = rtcClient.remoteVideoRef;
 
-  return <Video isLocal={false} name={name} videoRef={videoRef} />;
+  return (
+    <Video
+      isLocal={false}
+      name={rtcClient.remotePeerName}
+      videoRef={videoRef}
+    />
+  );
 };
 
 export default VideoRemote;
--- src/components/hooks/useRtcClient.js
+++ src/components/hooks/useRtcClient.js
@@ -1,9 +1,10 @@
-import React, { useState, useReducer, useEffect } from 'react';
+import React, { useState, useReducer, useEffect, useRef } from 'react';
 import RtcClient from '../../utils/RtcClient';
 
 const useRtcClient = () => {
   const [rtcClient, _setRtcClient] = useState(null);
   const [, forceRender] = useReducer((bool) => !bool, false);
+  const remoteVideoRef = useRef(null);
 
   const setRtcClient = (rtcClient) => {
     _setRtcClient(rtcClient);
@@ -12,7 +13,7 @@ const useRtcClient = () => {
 
   useEffect(() => {
     const init = async () => {
-      const client = new RtcClient(setRtcClient);
+      const client = new RtcClient(remoteVideoRef, setRtcClient);
       client.setMediaStream();
     };
     init();
