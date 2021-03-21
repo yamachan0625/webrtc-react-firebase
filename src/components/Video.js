f380a8edbf3c5c246e78dc97b2c091cd7fe6a635
yamachan0625 <napoli.080625@gmail.com>
2021-03-21 19:12:37 +0900

    dynamicVideoWidthChange

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import useDimensions from './hooks/useDimensions';
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
-});
+const useStyles = makeStyles({});
 
 const Video = ({ isLocal, name, videoRef }) => {
   const classes = useStyles();
+  const refCard = useRef(null);
+  const dimensionsCard = useDimensions(refCard);
 
   return (
-    <Card className={classes.root}>
+    <Card ref={refCard}>
       <CardActionArea>
-        <video autoPlay muted={isLocal} ref={videoRef} />
+        <video
+          width={dimensionsCard.width}
+          autoPlay
+          muted={isLocal}
+          ref={videoRef}
+        />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
             {name}
---
+++ src/components/hooks/useDimensions.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+
+const defaultDimensions = { width: 0, height: 0 };
+const useDimensions = (targetRef) => {
+  let [dimensions, setDimensions] = useState(defaultDimensions);
+  const node = targetRef.current;
+
+  const updateDimensions = (node) => {
+    return node === null
+      ? defaultDimensions
+      : {
+          width: node.offsetWidth,
+          height: node.offsetheight,
+        };
+  };
+  dimensions = updateDimensions(node);
+
+  useEffect(() => {
+    const resizeDimensions = () => {
+      setDimensions(updateDimensions(node));
+    };
+    window.removeEventListener('resize', resizeDimensions);
+    window.addEventListener('resize', resizeDimensions);
+  }, [node]);
+
+  return dimensions;
+};
+
+export default useDimensions;
